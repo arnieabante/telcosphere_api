e22a9ad5760eaac817a1d48544444fdcefa25d67
Arnie Abante <2PH362@IKSCELPT00161.SGPCLOUD.INNODATA.NET>
2025-10-22 08:36:19 +0800

    Update

--- database/factories/ModuleFactory.php
+++ database/factories/ModuleFactory.php
@@ -22,6 +22,7 @@ class ModuleFactory extends Factory
         return [
             'uuid' => fake()->uuid(),
             'site_id' => 1,
+            'icon' => "fa fa-edit",
             'name' => fake()->unique()->randomElement(['tickets', 'billing', 'reports', 'employees']),
             'description' => fake()->text(50),
             'is_active' => rand(0, 1),
