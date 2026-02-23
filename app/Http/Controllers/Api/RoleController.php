eaf43a8ffa151cf1df4c9ec35e8fbdc12947d73d
Abante <2PH362@IKSCELPT0013499.SGPCLOUD.INNODATA.NET>
2026-02-23 23:58:11 +0800

    Update controller for employee, role and user to add date filter

--- app/Http/Controllers/Api/RoleController.php
+++ app/Http/Controllers/Api/RoleController.php
@@ -27,8 +27,15 @@ class RoleController extends ApiController
         $perPage = $request->get('per_page', 10);
         $search = $request->get('search');
         $include = $request->get('include');
+        $from = $request->get('from');
+        $to = $request->get('to');
 
         $query = Role::query()->where('is_active', '=', '1');
+        
+        // Filter by date range
+        if (!empty($from) && !empty($to)) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
 
         if (!empty($include) && $include == 'all') {
             $roles = $query->orderBy('name', 'asc')->get();
