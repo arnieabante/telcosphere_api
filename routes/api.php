aa162eff55aa41873ccb373a5fb0651418e598d1
Abante <2PH362@IKSCELPT0013499.SGPCLOUD.INNODATA.NET>
2026-02-25 10:58:46 +0800

    update api for site controller

--- routes/api.php
+++ routes/api.php
@@ -95,5 +95,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('expenseitems', ExpenseItemController::class)->except(['update']);
     Route::patch('expenseitems/{uuid}', [ExpenseItemController::class, 'update']);
     Route::put('expenseitems/{uuid}', [ExpenseItemController::class, 'replace']);
+    
+    Route::apiResource('sites', SiteController::class)->except(['update']);
 
 });
