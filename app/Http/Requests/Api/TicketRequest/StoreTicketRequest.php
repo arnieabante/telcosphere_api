3de7b73613ffe2fcaebe82d60f60356a5d9398c7
Abante <2PH362@IKSCELPT0013499.SGPCLOUD.INNODATA.NET>
2026-02-27 18:23:12 +0800

    Update ticket to add ticket type and requestor name

--- app/Http/Requests/Api/TicketRequest/StoreTicketRequest.php
+++ app/Http/Requests/Api/TicketRequest/StoreTicketRequest.php
@@ -22,7 +22,9 @@ class StoreTicketRequest extends BaseTicketRequest
     public function rules(): array
     {
         return [
-            'clientId' => 'required|string',
+            'ticketType' => 'required|string',
+            'clientId' => 'nullable|string',
+            'requestorName' => 'required|string|max:50',
             'name' => 'required|string|max:50',
             'description' => 'nullable|string|max:100',
             'categoryId' => 'required|string',
