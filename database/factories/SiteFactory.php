d1bd07c1a6db9256f4ff7344f9df72d737ff6413
Abante <2PH362@IKSCELPT0013499.SGPCLOUD.INNODATA.NET>
2026-02-28 14:54:30 +0800

    update site id 1 to actual url

--- database/factories/SiteFactory.php
+++ database/factories/SiteFactory.php
@@ -38,7 +38,7 @@ class SiteFactory extends Factory
             [
                 'company_logo' => null,
                 'company_banner' => null,
-                'site_url' => "localhost:5173",
+                'site_url' => "app.telcosphere.co",
                 'company_name' => 'AbanteSoft',
                 'company_address' => 'Cebu, City',
                 'company_email' => 'abantesoft@telcosphere.co',
