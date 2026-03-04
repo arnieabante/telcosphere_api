ed4d42a1c2b7b106adac6bbc062320b35c201c88
Abante <2PH362@IKSCELPT0013499.SGPCLOUD.INNODATA.NET>
2026-03-04 17:26:13 +0800

    Update internet plan to restrict same name per site or client

--- app/Http/Requests/Api/InternetplanRequest/StoreInternetplanRequest.php
+++ app/Http/Requests/Api/InternetplanRequest/StoreInternetplanRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Api\InternetplanRequest;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreInternetplanRequest extends BaseInternetplanRequest
 {
