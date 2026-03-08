c296e3f4ace0635adac6e8a49b8e12fca5a72756
Abante <2PH362@IKSCELPT0013499.SGPCLOUD.INNODATA.NET>
2026-03-08 20:19:01 +0800

    Update base client request

--- app/Http/Requests/Api/ClientRequest/ReplaceClientRequest.php
+++ app/Http/Requests/Api/ClientRequest/ReplaceClientRequest.php
@@ -42,7 +42,7 @@ class ReplaceClientRequest extends BaseClientRequest
             'prorateFeeStatus' => 'sometimes|string',
             'inactiveDate' => 'sometimes|string|min:5',
             'notes' => 'sometimes|nullable|string|min:2',
-            'facebookProfileUrl' => 'sometimes|nullable|string|min:5',
+            'facebookProfileURL' => 'sometimes|nullable|string|min:5',
             'billingCategoryId' => 'sometimes|required|int',
             'serverId' => 'sometimes|required|int',
             'internetPlanId' => 'sometimes|required|int',
