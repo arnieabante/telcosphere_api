9702ec2530608cb6136987327659601184fb4e82
jeb saldariega <saldariega.jam@gmail.com>
2026-03-19 01:18:06 +0800

    applied incrementing of Invoice Number base on site config

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -10,16 +10,8 @@ class Invoice extends Model
         'invoice_number'
     ];
 
-    public function formatInvoiceNumber()
+    public function formatInvoiceNumber($prefix, $suffix)
     {
-        $site = Site::select(['invoice_id_pattern'])
-            ->where('id', auth()->user()->site_id)
-            ->first();
-
-        $prefix = strlen(trim($site['invoice_id_pattern'])) > 0 ?
-            $site['invoice_id_pattern'] . '-' :
-            '';
-
-        return $prefix . date('y') . str_pad($this->id, 6, '0', STR_PAD_LEFT);
+        return $prefix . date('y') . str_pad($suffix, 6, '0', STR_PAD_LEFT);
     }
 }
--- app/Services/InvoiceService.php
+++ app/Services/InvoiceService.php
@@ -3,15 +3,67 @@
 namespace App\Services;
 
 use App\Models\Invoice;
+use App\Models\Site;
 use Illuminate\Support\Facades\DB;
 
 class InvoiceService
 {
+    private $site;
+    private $prefix;
+    private $suffix;
+
+    public function __construct() 
+    {
+        $this->site = Site::select([
+                'invoice_id_pattern',
+                'invoice_id_yy_last_count'
+            ])
+            ->where('id', auth()->user()->site_id)
+            ->first();
+
+        $this->setPrefix();
+        $this->setSuffix();
+    }
+
+    private function setPrefix(): Void
+    {
+        $this->prefix = strlen(trim(
+                $this->site['invoice_id_pattern']
+            )) > 0 ?
+            $this->site['invoice_id_pattern'] . '-' :
+            '';
+    }
+
+    private function setSuffix(): Void 
+    {
+        // if new year reset count
+        $maxDate = Invoice::max('created_at');
+        $site = Site::where('id', auth()->user()->site_id)->first();
+
+        if ($maxDate && 
+            (int) date('y') > (int) date('y', strtotime($maxDate))
+        ) {
+            $site->update([
+                'invoice_id_yy_last_count' => 0
+            ]);
+        }
+
+        $this->suffix = ((int) $site['invoice_id_yy_last_count']) + 1;
+        
+        // update new count
+        $site->update([
+            'invoice_id_yy_last_count' => $this->suffix
+        ]);
+    }
+
     public function generateInvoice(): Invoice
     {
         return DB::transaction(function () {
             $invoice = Invoice::create();
-            $invoiceNumber = $invoice->formatInvoiceNumber();
+            $invoiceNumber = $invoice->formatInvoiceNumber(
+                $this->prefix, 
+                $this->suffix
+            );
 
             $invoice->update([
                 'invoice_number' => $invoiceNumber
