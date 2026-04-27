deaf8ca9f58269415b979e47667a8be58fdbd409
jeb saldariega <saldariega.jam@gmail.com>
2026-04-27 16:29:45 +0800

    removed creation of billing item for balance from previous billing

--- app/Libraries/Billing/MonthlySubscription.php
+++ app/Libraries/Billing/MonthlySubscription.php
@@ -71,27 +71,7 @@ class MonthlySubscription implements BillingInterface
             }
         }
 
-        // create billing item if there is balance from previous billing
-        if ($billing->balance_from_prev_billing > 0) {
-            $prevBillingItem = $this->generatePrevBalanceBillingItem($billing->balance_from_prev_billing);
-            return array_merge([$prevBillingItem], $data);
-
-        } else
-            return $data; 
-    }
-
-    protected function generatePrevBalanceBillingItem($balance) : array {
-        return [
-            'billing_item_name' => self::ITEM_NAME_BALANCE_FROM_PREV_BILLING,
-            'billing_item_particulars' => self::ITEM_NAME_BALANCE_FROM_PREV_BILLING,
-            'billing_item_quantity' => 1,
-            'billing_item_price' => $balance,
-            'billing_item_amount' => $balance * 1,
-            'billing_item_offset' => '0.00',
-            'billing_item_balance' => $balance * 1,
-            'billing_item_remark' => NULL,
-            'billing_status' => self::ITEM_STATUS_DEFAULT
-        ];
+        return $data; 
     }
 
     protected function getSubscriptionRate(string $planId): float {
